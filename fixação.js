6cb8406b082c76f112aaf09649ad6fbea855ef2f
Manuela Edvania <87840450+MEdvania@users.noreply.github.com>
2021-11-22 20:50:14 -0300

    Add files via upload

---
+++ fixação.js
@@ -0,0 +1,49 @@
+// ATIVIDADE 1 
+
+// 1) Criar variáveis ​​com tipos de dados:
+
+//um número
+
+var  numero  =  10 ;
+
+// b) String
+
+var  nome  =  "João" ;
+
+// c) Booleano
+
+var  vedade  =  verdadeiro ;
+var  mentira  =  falso ;
+
+// d) Array
+
+var  frutas  =  [ "maçã" ,  "pera" ,  "uva" ] ;
+
+// e) Objeto
+
+var  carro  =  { cor : "preto" ,  modelo : "XC60" , marca : "Volvo" ,  ano : 2020 } ;
+
+// f) Indefinido
+
+var  a ;
+
+// g) Nulo 
+
+var  nul  =  nulo ;
+
+
+// ATIVIDADE 2
+
+// 1) Criar duas variáveis ​​usando uma palavra-chave var.
+
+ var  nome  =  "João" ;
+ var  animal  =  "cachorro" ;
+
+// 2) Criar duas variáveis ​​usando uma palavra-chave let.
+
+let  idade  =  18 ;
+
+
+// 3) Criar duas constantes usando a palavra-chave const.
+
+const  cidade  =  "Recife" ;
